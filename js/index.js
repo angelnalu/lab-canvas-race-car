6de759ad1f338a7c3e27a9993f770b62104eea08
Nayhara Luiza <nlm.nalu@yahoo.com.br>
2021-03-18 12:13:45 -0400

    finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,179 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+window.onload = function() {
+  let newIslandRacer;
+  document.getElementById("start-button").onclick = function() {
     startGame();
+    if( document.getElementById("canvas").length === 0 ) {
+      resetGame();
+    } else {
+      startGame();
+    }
   };
+  function startGame() {
+    newIslandRacer = new IslandRacer( "game-board", 450, 600 );
+    document.getElementById("start-button").innerText = "Reset Game";
+  }
 
-  function startGame() {}
+  function resetGame() {
+    newIslandRacer.stopGame();
+    document.getElementById("start-button").innerText = "Start Game";
+    document.getElementById("game-board").innerHTML = "";
+  }
 };
+class CanvasGame {
+  constructor(container, width, height) {
+    this.canvas = document.getElementById("canvas");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.ctx = this.canvas.getContext("2d");
+    document.getElementById(container).appendChild(this.canvas);
+    this.ctx.rect(0, 0, width, height);
+  }
+}
+class MovingObject{
+  constructor(x, y, width, height, color, ctx){
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.ctx = ctx;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.acceleration = 5;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  left() { console.log(this.x); return this.x; }
+  right() { return this.x + this.width; }
+  top() { return this.y; }
+  bottom() { return this.y + this.height; }
+
+  isCollidedWith(obstacle) {
+    //since the player is also a gameObject we have to make sure that it doesn't "collide" with itself
+    if (this === obstacle) return false;
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+  }
+}
+class RacingCar extends MovingObject{
+  constructor(x, y, ctx){
+    super( x-35, y-140, 70, 140, "red", ctx );
+    document.onkeydown = e => {
+      switch (e.keyCode) {
+        case 37:
+          this.speedX -= (1 * this.acceleration);
+          break;
+        case 39:
+          this.speedX += (1 * this.acceleration);
+          break;
+        default:
+      }
+    };
+    document.onkeyup = e => {
+      this.speedX = 0;
+    };
+    this.raceCarImage = new Image();
+    this.raceCarImage.src = "./images/car.png";
+  }
+  update(){
+    this.x += this.speedX;
+    this.ctx.drawImage(this.raceCarImage,0,0,this.raceCarImage.width, this.raceCarImage.height, this.x, this.y, this.width, this.height);
+  }
+}
+class Obstacle extends MovingObject {
+  constructor(x, width, ctx) {
+    super(x, 0, width, 20, "yellow", ctx);
+    this.speedX = 0;
+    this.speedY = 3;
+
+    this.obstacleImage = new Image();
+    this.obstacleImage.src = "./images/barrier.png";
+
+    this.obstacleImage.onload = () => {
+      this.obstaclePattern = this.ctx.createPattern(this.obstacleImage, 'repeat');
+    };
+  }
+  update(){
+    this.y += this.speedY;
+    this.ctx.fillStyle = this.obstaclePattern;
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+class IslandRacer extends CanvasGame{
+  constructor(container, width, height){
+    super(container, width, height);
+    this.frames = 0;
+
+    // bind func 'updateIslandRacerState' to be available in 'setInterval()'
+    this.updateIslandRacerState = this.updateIslandRacerState.bind(this);
+
+    // create & init timer
+    this.interval = setInterval(this.updateIslandRacerState, 15);
+    this.gamesObjects = [];
+
+    // create racing car
+    this.racingCar = new RacingCar(this.canvas.width/2, this.canvas.height-10, this.ctx);
+    this.gamesObjects.push(this.racingCar);
+
+    // init middleline
+    this.middleLineOffset = 0;
+  }
+  addObstacle() {
+    // console.log(this);
+    let minWidth = 100;
+    let maxWidth = 300;
+    let width = Math.floor( Math.random() * (maxWidth - minWidth + 1) + minWidth );
+    let x = Math.floor( Math.random() * ( this.canvas.width-width -60 ) ) + 30;
+    this.gamesObjects.push(new Obstacle(x, width, this.ctx));
+  }
+  stopGame(){
+    clearInterval(this.interval);
+  }
+  clearCanvas(){
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  checkGameOver(){
+    let crashed=this.gamesObjects.some(obstacle => { return this.racingCar.isCollidedWith(obstacle); });
+    let carOutOfRoad = ( this.racingCar.x < 20 || this.racingCar.x > (this.canvas.width - this.racingCar.width - 20));
+    if (crashed || carOutOfRoad) this.stopGame();
+  }
+  drawRoad(){
+    this.ctx.fillStyle = "green";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "grey";
+    this.ctx.fillRect(30, 0, this.canvas.width-60, this.canvas.height);
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(40, 0, 10, this.canvas.height);
+    this.ctx.fillRect(this.canvas.width-50, 0, 10, this.canvas.height);
+
+    // middle line
+    this.ctx.setLineDash([20, 20]);
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = "white";
+    this.ctx.beginPath();
+      this.ctx.moveTo(this.canvas.width/2,(this.middleLineOffset%40)-20)
+      this.ctx.lineTo(this.canvas.width/2,this.canvas.height)
+    this.ctx.stroke();
+    this.middleLineOffset += 3;
+  }
+  updateIslandRacerState(){
+    this.frames += 1;
+    if (this.frames % 150 === 0) {
+      this.addObstacle();
+    }
+    this.clearCanvas();
+    this.drawRoad();
+
+    this.gamesObjects.forEach(gameObject => gameObject.update());
+
+    this.checkGameOver();
+  }
+}
